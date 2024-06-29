b9827cf9856e54707f1780d01e461d0ac182efff
Oleksii Sodolinskyi <sodolinskyi@gmail.com>
2024-06-29 13:04:04 -0700

    added useState

--- src/containers/AddTodo.jsx
+++ src/containers/AddTodo.jsx
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { addTodo } from "../actions";
 
 const AddTodo = ({ dispatch }) => {
-  let input;
+  const [input, setInput] = useState("");
 
   return (
     <div>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          if (!input.value.trim()) {
+          if (!input.trim()) {
             return;
           }
-          dispatch(addTodo(input.value));
-          input.value = "";
+
+          dispatch(addTodo(input));
+          setInput("");
         }}
       >
-        <input ref={(node) => (input = node)} />
+        {input}
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
         <button type='submit'>Add Todo</button>
       </form>
     </div>
